b4d3cc05cc941fa885b8a3a8a89e110fb14752d7
Suyash <suyashrusia2005@gmail.com>
2024-02-21 15:17:31 +0530

    need to make it responsive.

--- script.js
+++ script.js
@@ -78,3 +78,161 @@ gsap.to(".page-5-head",{
     y: -70,
     duration: 1,
 });
+
+var tl3 = gsap.timeline({
+    scrollTrigger:{
+        toggleActions:"play none none reset",
+        trigger: ".ll1",
+        start: "top 100%",   
+    },
+});
+
+tl3.to(".ll1>.int-1",{
+    y: -50,
+    duration: 0.3,
+})
+.to(".ll1>.int-2",{
+    y: -50,
+    duration: 0.3,
+})
+.to(".ll1>.int-3",{
+    y: -50,
+    duration: 0.3,
+})
+var tl4 = gsap.timeline({
+    scrollTrigger:{
+        toggleActions:"play none none reset",
+        trigger: ".ll2",
+        start: "top 90%",   
+    },
+});
+tl4.to(".ll2>.int-1",{
+    y: -50,
+    duration: 0.3,
+})
+.to(".ll2>.int-2",{
+    y: -50,
+    duration: 0.3,
+})
+.to(".ll2>.int-3",{
+    y: -50,
+    duration: 0.3,
+});
+
+gsap.to(".im-1",{
+    scrollTrigger:{
+        start: "top 90%",
+        toggleActions:"play none none reset",
+        trigger: ".b1",
+    },
+    x: "20rem",
+    duration: 1, 
+});
+gsap.to(".im-3",{
+    scrollTrigger:{
+        start: "top 90%",
+        toggleActions:"play none none reset",
+        trigger: ".b3",
+    },
+    x: "20rem",
+    duration: 1, 
+});
+gsap.to(".im-5",{
+    scrollTrigger:{
+        start: "top 90%",
+        toggleActions:"play none none reset",
+        trigger: ".b5",
+    },
+    x: "20rem",
+    duration: 1, 
+});
+gsap.to(".cd-2",{
+    scrollTrigger:{
+        start: "center 90%",
+        toggleActions:"play none none reset",
+        trigger: ".b2",
+    },
+    x: "20rem",
+    duration: 1, 
+});
+gsap.to(".cd-4",{
+    scrollTrigger:{
+        start: "top 90%",
+        toggleActions:"play none none reset",
+        trigger: ".b4",
+    },
+    x: "20rem",
+    duration: 1, 
+});
+gsap.to(".cd-6",{
+    scrollTrigger:{
+        start: "top 90%",
+        toggleActions:"play none none reset",
+        trigger: ".b6",
+    },
+    x: "20rem",
+    duration: 1, 
+});
+gsap.to(".im-2",{
+    scrollTrigger:{
+        start: "top 90%",
+        toggleActions:"play none none reset",
+        trigger: ".b2",
+    },
+    x: "-20rem",
+    duration: 1, 
+});
+gsap.to(".im-4",{
+    scrollTrigger:{
+        start: "top 90%",
+        toggleActions:"play none none reset",
+        trigger: ".b4",
+    },
+    x: "-20rem",
+    duration: 1, 
+});
+gsap.to(".im-6",{
+    scrollTrigger:{
+        start: "top 90%",
+        toggleActions:"play none none reset",
+        trigger: ".b6",
+    },
+    x: "-20rem",
+    duration: 1, 
+});
+gsap.to(".cd-1",{
+    scrollTrigger:{
+        start: "center 90%",
+        toggleActions:"play none none reset",
+        trigger: ".b1",
+    },
+    x: "-20rem",
+    duration: 1, 
+});
+gsap.to(".cd-3",{
+    scrollTrigger:{
+        start: "top 90%",
+        toggleActions:"play none none reset",
+        trigger: ".b3",
+    },
+    x: "-20rem",
+    duration: 1, 
+});
+gsap.to(".cd-5",{
+    scrollTrigger:{
+        start: "top 90%",
+        toggleActions:"play none none reset",
+        trigger: ".b5",
+    },
+    x: "-20rem",
+    duration: 1, 
+});
+gsap.to(".img-1",{
+    scrollTrigger:{
+        start: "top 90%",
+        toggleActions:"play none none reset",
+        trigger: ".page-4-main",
+    },
+    x: "20rem",
+    duration: 1, 
+});
